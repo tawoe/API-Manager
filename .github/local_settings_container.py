dba1558597401daefbb71eafc7ac2bd9dd6a4e49
tawoe <weserwueter@posteo.de>
2022-05-20 13:58:15 +0200

    create container image WIP

---
+++ .github/local_settings_container.py
@@ -0,0 +1,15 @@
+import os
+
+
+if 'OAUTH_CONSUMER_KEY' in os.environ:
+	OAUTH_CONSUMER_KEY = os.environ['OAUTH_CONSUMER_KEY']
+if 'OAUTH_CONSUMER_SECRET' in os.environ:
+	OAUTH_CONSUMER_SECRET = os.environ['OAUTH_CONSUMER_SECRET']
+if 'SECRET_KEY' in os.environ:
+	SECRET_KEY = os.environ['SECRET_KEY']
+if 'API_HOST' in os.environ:
+	API_HOST = os.environ['API_HOST']
+if 'API_PORTAL' in os.environ:
+	API_PORTAL = os.environ['API_PORTAL']
+if 'VERIFY' in os.environ:
+	VERIFY = os.environ['VERIFY']
